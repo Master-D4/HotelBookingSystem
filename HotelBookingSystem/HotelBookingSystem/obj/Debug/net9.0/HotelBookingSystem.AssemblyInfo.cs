50b679817b39e224c9fabdd53e9e1708f2860d13
Dexora Solutions <dexorasolutions@gmail.com>
2025-06-29 19:32:55 +0530

    base flow set

--- HotelBookingSystem/HotelBookingSystem/obj/Debug/net9.0/HotelBookingSystem.AssemblyInfo.cs
+++ HotelBookingSystem/HotelBookingSystem/obj/Debug/net9.0/HotelBookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c898ee17487586fbcec5ad0f98cf6462bc96c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf5c67853d67b5ecb4cf7b00e8aeed684e9446a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
