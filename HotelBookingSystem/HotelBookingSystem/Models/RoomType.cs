9a5ea71b27e17c3a3f55631691378171cac3f8b1
Dexora Solutions <dexorasolutions@gmail.com>
2025-06-30 18:56:32 +0530

    Room Type Management flow add

--- HotelBookingSystem/HotelBookingSystem/Models/RoomType.cs
+++ HotelBookingSystem/HotelBookingSystem/Models/RoomType.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelBookingSystem.Models;
 
 public class RoomType
 {
+    [Required(ErrorMessage = "Room type name is required")]
     public string Name { get; set; }
+    
+    [Required(ErrorMessage = "Base price is required")]
+    [Range(1, 1000000, ErrorMessage = "Price must be between 1 and 1000000")]
     public decimal BasePrice { get; set; }
 }
